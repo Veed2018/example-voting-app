62a3c82c53f5881fa1fbebd2540663094e886b77
Eduard Veichel <eduard.veichel1@ibm.com>
2023-09-21 19:47:20 +0300

    updated checkov script

--- iaac/azure/checkov.sh
+++ iaac/azure/checkov.sh
@@ -22,7 +22,7 @@ WORK_DIR=${1-$(pwd)}
 #######################################
 run_checkov() {
   local test_dir=$1
-  docker run -t -v "${test_dir}":/tf bridgecrew/checkov -d /tf
+  docker run -t -v "${test_dir}":/tf bridgecrew/checkov -f /tf/terraform.tfplan
 }
 
 #######################################
